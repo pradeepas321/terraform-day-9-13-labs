0248d6eb4847535b23abf49168d22ef3fa5a0849
pradeepas321 <antonypradeep321@gmail.com>
2026-06-26 13:21:26 +0000

    Remove backend.tf file

--- projects/apple/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-apple-dev" # Replace with your bucket name
-    key          = "apple/dev/terraform.tfstate"
-    region       = "ap-south-1"
-    encrypt      = true
-  }
-}
