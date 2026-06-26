aa9631d5db8241cefdf9af60f08e0be566489443
pradeepas321 <antonypradeep321@gmail.com>
2026-06-26 14:04:56 +0000

    updating lock file

--- projects/apple/dev/backend.tf
+++ projects/apple/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-apple-dev"   # Replace with your actual bucket
     key            = "apple/dev/terraform.tfstate"    # Path inside the bucket
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"                # DynamoDB table for state locking
+#    dynamodb_table = "terraform-locks"                # DynamoDB table for state locking
     encrypt        = true
   }
 }
